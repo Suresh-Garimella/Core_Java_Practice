c815a04f1728d361841be6a5a90c195ee90e984c
Suresh-Garimella <112640521+Suresh-Garimella@users.noreply.github.com>
2024-01-14 03:14:05 -0600

    3 javafiles added related to static,final and constructor

---
+++ static_final_constructor/Instance_Var.java
@@ -0,0 +1,36 @@
+package static_final_constructor;
+
+public class Instance_Var {
+    public static void main(String[] args) {
+        Child obj = new Child();
+        System.out.println("Initialized usign default Constructor");
+        System.out.println(
+                "instance variable val in Child Class  :" + obj.j + "\ninstance variable val in Parent Class  :"
+                        + obj.i);
+
+        Child obj1 = new Child(16);
+        System.out.println("Initialized usign parameterized Constructor");
+        System.out.println(
+                "instance variable val in Child Class  :" + obj1.j + "\ninstance variable val in Parent Class  :"
+                        + obj1.i);
+    }
+}
+
+class Parent {
+    int i;
+}
+
+class Child extends Parent {
+    int j;
+
+    Child() {
+
+        super.i = 20;
+        this.j = 10;
+    }
+
+    Child(int j) {
+        super.i = 15;
+        this.j = j;
+    }
+}
