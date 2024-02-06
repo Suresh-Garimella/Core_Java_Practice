3fb87f9ad9ac122953849f34a09a081ab5579975
Suresh-Garimella <112640521+Suresh-Garimella@users.noreply.github.com>
2024-02-06 15:30:39 -0600

    Junit for Exceptional Handling

--- src/main/java/exceptionHandling/StringToInt.java
+++ src/main/java/exceptionHandling/StringToInt.java
@@ -4,16 +4,19 @@ package exceptionHandling;
 // sample inputs - "23", "45.67" , "test", "123f"
 public class StringToInt   {
 
-    public static String convertStringToInt(String str) throws NumberFormatException {
-        try {
-            return ""+Integer.parseInt(str);
-        }
-        catch(NumberFormatException ex){
-            return "Inside the Exception \t"+ex.getMessage();
-        }
+    public static Integer convertStringToInt(String str) throws NumberFormatException {
+
+            return Integer.parseInt(str);
     }
 
-    public static void main(String[] args) {
-        System.out.println( StringToInt.convertStringToInt("abc"));
-    }
+//    public static void main(String[] args) {
+//        System.out.println( StringToInt.convertStringToInt("45.67"));
+//
+//        System.out.println( StringToInt.convertStringToInt("test"));
+//
+//        System.out.println( StringToInt.convertStringToInt("23"));
+//
+//        System.out.println( StringToInt.convertStringToInt("123f"));
+//
+//    }
 }
